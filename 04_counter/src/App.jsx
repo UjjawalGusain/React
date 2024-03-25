05aded207dc8ca3761dc27dc606e538b1cef3431
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-03-25 21:01:26 +0530

    Added snapshot function for counter

--- 04_counter/src/App.jsx
+++ 04_counter/src/App.jsx
@@ -11,9 +11,30 @@ function App() {
     console.log("Button clicked: ", counter);
   }
 
+  const add2Button = () => {
+    if(counter == 20) return;
+    if(counter == 19){
+      setCounter(prevCounter => prevCounter + 1)
+      return
+    }
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+  }
+
   const removeButton = () => {
     if(counter == 0) return;
     setCounter(counter - 1)
+  }
+
+  // To learn how to use react snapshots
+  const remove2Button = () => {
+    if(counter == 0) return;
+    if(counter == 1){
+      setCounter(prevCounter => prevCounter - 1)
+      return
+    }
+    setCounter(prevCounter => prevCounter - 1)
+    setCounter(prevCounter => prevCounter - 1)
   }
 
   return (
@@ -23,7 +44,12 @@ function App() {
 
       <button onClick={addButton}>Add</button>
       <br /><br />
+      <button onClick={add2Button}>Add2Button</button>
+      <br /><br />
       <button onClick={removeButton}>Remove</button>
+      <br /><br />
+      <button onClick={remove2Button}>Remove2Button</button>
+      
     </>
   )
 }
